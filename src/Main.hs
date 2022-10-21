19055a29f4001f433f2b1e6dfdf9d42f5863aa5b
SnO2WMaN <me@sno2wman.net>
2022-10-22 01:53:53 +0900

    ノコギリ波と矩形波

--- src/Main.hs
+++ src/Main.hs
@@ -39,20 +39,29 @@ f x = a x - 4.0
 g :: Float -> Semitones
 g x = a x - 2.0
 
+squareWave :: Int -> Float -> Float
+squareWave n t = sum $ (\k -> sin ((2 * fromIntegral k - 1) * t) / (2 * fromIntegral k - 1)) <$> [1 .. n]
+
+sawWave :: Int -> Float -> Float
+sawWave n t = sum $ (\k -> sin (fromIntegral k * t) / fromIntegral k) <$> [1 .. n]
+
 type Pulse = Float
 frequency :: Volume -> Hz -> Seconds -> [Pulse]
 frequency volume hz duration = (* volume) <$> zipWith3 (\a b -> (a * b *)) attack release freq
   where
     step = (hz * 2 * pi) / sampleRate
     freq :: [Pulse]
-    freq = sin . (* step) <$> [0.0 .. sampleRate * duration]
+    freq = sawWave 25 . (* step) <$> [0.0 .. sampleRate * duration]
     attack :: [Pulse]
-    attack = min 1.0 <$> [0, 0.005 ..]
+    attack = min 1.0 <$> [0, 0.01 ..]
     release :: [Pulse]
     release = reverse $ take (length freq) attack
 
 note :: Volume -> BPM -> Semitones -> Beats -> [Float]
 note volume bpm n = frequency volume (semitonesToHz n) . beatsToSeconds bpm
+
+chord :: [Pulse] -> [Pulse] -> [Pulse]
+chord = zipWith (+)
 
 save :: FilePath -> [Pulse] -> IO ()
 save filepath wave = writeFileLBS filepath $ B.toLazyByteString $ foldMap B.floatLE wave
@@ -62,7 +71,7 @@ play filepath = callProcess "ffplay" ["-f", "f32le", "-nodisp", "-autoexit", "-a
 
 main :: IO ()
 main = do
-  let noteT = note 0.01 140
+  let noteT = note 0.05 140
   save "sound" $
     concat
       [ noteT (e 5) 1.0
